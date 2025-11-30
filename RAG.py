66c096bad600e83487e20bcac5eebea43899ddbe
VonaviArt <snusmumric782@gmail.com>
2025-11-30 13:55:36 +0300

    Update RAG.py
    вбд теперь chromadb, есть ретривер

--- RAG.py
+++ RAG.py
@@ -1,65 +1,67 @@
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from openai import OpenAI
-import lancedb as ld
-import pyarrow as pa
+from chromadb import Client
+from chromadb.config import Settings
+import chromadb
+from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 import os
 
-
-file_path = r"C:\Users\Lenovo\Desktop\RAG\RAG_инфа по распорядку и пр.pdf"
+# загрузка пдф
+file_path = r"C:\Users\Lenovo\Desktop\RAG\RAG_инфа по распорядку и пр.pdf" #заменить позже
 loader = PyPDFLoader(file_path)
-
 docs = loader.load()
-# for doc in docs:
-#     print(doc.page_content) #грузанули в docs
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
-all_texts = []#все страницы отчанковали
+# разбиение текста на чанки
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=100)
+all_texts = []
 for doc in docs:
     chunks = text_splitter.split_text(doc.page_content)
     all_texts.extend(chunks)
 
-
-#эмбеддинги прям по видео, но есть кучи моделек на русском
+# создание эмбеддингов
 hf_embeddings_model = HuggingFaceEmbeddings(model_name="cointegrated/LaBSE-en-ru", model_kwargs={"device": "cpu"})
 embeddings = hf_embeddings_model.embed_documents(all_texts)
 
-#оки-доки, теперь вбд
-db = ld.connect(r"C:\Users\Lenovo\Desktop\RAG\db")
-schema = pa.schema([ #lance не принимает схемы без pyarrow
-    pa.field("id", pa.int64()),
-    pa.field("text", pa.string()),
-    pa.field("embedding", pa.list_(pa.float32(), 768))
-])
-try:
-    table = db.open_table("documents")
-except Exception:
-    table = db.create_table("documents", schema=schema)
-# вставляем данные
-records = []
-for i in range(len(all_texts)):
-    record = {
-        "id": i,
-        "text": all_texts[i],
-        "embedding": embeddings[i]
-    }
-    records.append(record)
-table.add(records) # и добавим в таблицу
-my_text = "Научный читальный зал (НЧЗ) Понедельник - пятница со скольки до скольки работает"# У - уникальность
-my_text_embedded = hf_embeddings_model.embed_query(my_text) #перевели в эмбеддинг запрос\
-# найдем похожие эмбеддинги в таблице
-results = table.search(
-    my_text_embedded
+# вбд
+client = chromadb.Client()
+collection = client.get_or_create_collection(name="documents")
+ids = [str(i) for i in range(len(all_texts))]
+collection.add(
+    ids=ids,
+    embeddings=embeddings,
+    documents=all_texts
 )
 
-# собираем все чанки в один контекст
-results_list = results.to_list()
-context = "\n\n".join(record["text"] for record in results_list)
-#апишка к чату гпт
+# эмбеддинг для запроса и поиск
+my_text = "сколько стипендия у иностранцев троечников?"
+my_text_embedded = hf_embeddings_model.embed_query(my_text)
+results = collection.query(
+    query_embeddings=[my_text_embedded],
+    n_results=2
+)
+
+# ретривер
+vector_store = Chroma(
+    collection_name="documents",
+    embedding_function=hf_embeddings_model,
+)
+retriever = vector_store.as_retriever()
+retriever_results = vector_store.similarity_search_with_score(my_text)
+
+# контекст
+context = "\n\n".join(
+    d.page_content if hasattr(d, 'page_content') else str(d)
+    for d, score in retriever_results
+)
+print("Найденный контекст для запроса:")
+print(context, "/n")
+
+# Апи к ИИ
 client = OpenAI(
     base_url="https://router.huggingface.co/v1",
-    api_key='-...-'
+    api_key=""# спрятать не в коде потом
 )
 
 completion = client.chat.completions.create(
@@ -67,7 +69,7 @@ completion = client.chat.completions.create(
     messages=[
         {
             "role": "system",
-            "content": "ты - помощник Игорь. ты заикаешся и занудствуешь. представься в начале. используй искючительно контекст для ответа."
+            "content": "ты - помощник Игорь. используй исключительно контекст для ответа."
         },
         {
             "role": "user",
@@ -76,6 +78,5 @@ completion = client.chat.completions.create(
     ],
 )
 
+print("Ответ модели:")
 print(completion.choices[0].message.content)
-
-
